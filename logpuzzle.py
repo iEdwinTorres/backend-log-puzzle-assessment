36cc1e4b1d96fd0a2ae16bc44c518b4cf798a4ac
Edwin Torres <edwin_torres@icloud.com>
2020-07-23 01:34:02 -0400

    Finished assessment.

--- logpuzzle.py
+++ logpuzzle.py
@@ -17,7 +17,7 @@ rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 import os
 import re
 import sys
-import urllib.request
+from urllib.request import urlretrieve
 import argparse
 
 
@@ -26,8 +26,34 @@ def read_urls(filename):
     extracting the hostname from the filename itself, sorting
     alphabetically in increasing order, and screening out duplicates.
     """
-    # +++your code here+++
-    pass
+    puzzle_urls = []
+    with open(filename, "r") as log_file:
+        log_list = log_file.read().split("\n")
+        log_list = list(filter(lambda x: "/puzzle/" in x, log_list))
+        for url in log_list:
+            url_result = re.findall(r"GET (\S+) HTTP", url)
+            puzzle_urls.append(url_result[0])
+    url_list = create_urls(puzzle_urls)
+    url_list = list(set(url_list))
+    sorted_urls = sorted(url_list, key=return_last_word)
+    return sorted_urls
+
+
+def create_urls(urls):
+    front = "http://code.google.com"
+    url_return = [front + url for url in urls]
+    return url_return
+
+
+def return_last_word(url):
+    return re.findall(r"-(....).jpg", url)
+
+
+def add_prefixes(filename, host_list):
+    """Adds server prefixes to urls in host list"""
+    server_name = "https://" + re.findall(r"\S+\_(\S+)", filename)[0]
+    completed_url_list = [server_name + host for host in host_list]
+    return completed_url_list
 
 
 def download_images(img_urls, dest_dir):
@@ -38,16 +64,28 @@ def download_images(img_urls, dest_dir):
     to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
-    pass
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+        print("dir made")
+    index_html = "<html><body>"
+    for index, url in enumerate(img_urls):
+        image_name = "img" + str(index)
+        print("Retriveing {}".format(url))
+        urlretrieve(url, dest_dir + "/" + image_name)
+        index_html += "<img src={}></img>".format(image_name)
+    index_html += "</body></html>"
+
+    with open(dest_dir + "/index.html", "w") as w_index:
+        w_index.write(index_html)
 
 
 def create_parser():
     """Creates an argument parser object."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir',
-                        help='destination directory for downloaded images')
-    parser.add_argument('logfile', help='apache logfile to extract urls from')
+    parser.add_argument(
+        "-d", "--todir", help="destination directory for downloaded images"
+    )
+    parser.add_argument("logfile", help="apache logfile to extract urls from")
 
     return parser
 
@@ -67,8 +105,8 @@ def main(args):
     if parsed_args.todir:
         download_images(img_urls, parsed_args.todir)
     else:
-        print('\n'.join(img_urls))
+        print("\n".join(img_urls))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
